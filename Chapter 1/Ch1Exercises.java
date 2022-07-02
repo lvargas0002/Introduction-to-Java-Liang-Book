4c1333c81a6a9ec3cccdff9d7dbe9b4e7fbc2434
lvargas0002 <lvargasma0002@gmail.com>
2022-07-01 19:26:41 -0700

    Create Ch1Exercises.java

---
+++ Chapter 1/Ch1Exercises.java
@@ -0,0 +1,58 @@
+public class Ch1Exercises {
+    public static void main(String[] args) {
+
+		// Exercise 1.1 display three messages
+		System.out.println("Exercise 1.1 : ");
+		System.out.println("Welcome to Java");
+		// Exercise 1.2 display welcome to java *5
+		System.out.println("\nExercise 1.2 : ");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+
+		// Exercise 1.3 display JAVA pattern
+		System.out.println("\nExercise 1.3 : ");
+		System.out.println("    J    A     V     V    A");
+		System.out.println("    J   A A     V   V    A A");
+		System.out.println("J   J  AAAAA     V V    AAAAA");
+		System.out.println(" J J  A     A     V    A     A");
+
+		// Exercise 1.4 display table
+		System.out.println("\nExercise 1.4 : ");
+		System.out.println("a     a^2     a^3");
+		System.out.println("1     1       1");
+		System.out.println("2     4       8");
+		System.out.println("3     9       27");
+		System.out.println("4     16      64");
+
+		// Exercise 1.5 write program displaying result of 9.5 X 4.5 - 2.5 X 3 / 45.5 -
+		// 3.5
+		System.out.println("\nExercise 1.5 : ");
+		System.out.println("9.5 x 4.5 - 2.5 x 3\n___________________ =\n    45.5 - 3.5");
+		System.out.println((9.5 * 4.5 - (2.5 * 3)) / (45.5 - 3.5));
+
+		// Exercise 1.6 write program that displays result of 1+2+3+4+5+7+8+9
+		System.out.println("\nExercise 1.6 : ");
+		System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = " + (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9));
+
+		// Exercise 1.7 compute pi with different formulas
+		System.out.println("\nExercise 1.7 : ");
+		System.out.println("Formula 1 :");
+		System.out.println("pi = " + (4 * 1.0 - 1 / 3 + 1 / 5 - 1 / 7 + 1 / 9 - 1 / 11));
+		System.out.println("Formula 2 :");
+		System.out.println("pi = " + (4 * 1.0 - 1 / 3 + 1 / 5 - 1 / 7 + 1 / 9 - 1 / 11 + 1 / 13));
+
+		// Exercise 1.8 Calculate the area and perimeter of a circle with radius 5.5
+		System.out.println("\nExercise 1.8 : ");
+		System.out.println("Perimeter = 2 x 5.5 x 3.14 = " + (2 * 5.5 * 3.14));
+		System.out.println("Area = 5.5 x 5.5 x 3.14 = " + (5.5 * 5.5 * 3.14));
+
+		// Exercise 1.9 calculate are and perimeter of rectangle with width 4.5 and height 7.9
+		System.out.println("\nExercise 1.9 : ");
+		System.out.println("Perimeter = 2 x (4.5 + 7.9) = " + (2 * (4.5 + 7.9)));
+		System.out.println("Area = 4.5 x 7.9 = " + (4.5 * 7.9));
+		
+	}
+}
